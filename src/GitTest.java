62f644a5e9715be3cb7d821f786e5649e03db2a3
Aliso4ka1313 <Vasilevskaya9090@mail.ru>
2015-03-08 00:12:25 +0400

    Initial commit

---
+++ src/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by PC XP on 07.03.15.
+ */
+public class GitTest {
+    public static void main (String args[]){
+        System.out.println("Works");
+        System.out.println("New commit");
+
+    }
+
+}
