7e38e45d992668a91f488c8a07a14058bf02564e
job.dmy@gmail.com <tKtrnhjy1610-19>
2015-06-10 17:39:00 +0300

    Mihael

--- src/GitTest.java
+++ src/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
     public static void main (String args[]){
         System.out.println("Works");
         System.out.println("New commit");
-
+        System.out.println("New commit");
     }
 
 }
